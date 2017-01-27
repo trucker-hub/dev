1f44abfdc946ae1464abbc7145469eb8e3c7ad64
jinbo chen <jinbo.chen@gmail.com>
2017-01-27 15:21:52 -0800

    add email mining placeholder

--- server/api/emails-mining/emails-mining.model.js
+++ server/api/emails-mining/emails-mining.model.js
@@ -2,9 +2,27 @@
 
 import mongoose from 'mongoose';
 
+var addressSchema = {
+  address: String,
+  name: String
+};
+
 var EmailsMiningSchema = new mongoose.Schema({
-  name: String,
-  info: String,
+  from: addressSchema,
+  subject: String,
+  headers : [ {key: String, value: String} ],
+  to: [ addressSchema ],
+  cc: [ addressSchema ],
+  bcc:[ addressSchema ],
+  date: Date,
+  messageID: String,
+  inReplyTo: String,
+  replyTo: String,
+  references: [String],
+  priority: { type: String, enum: [ 'high', 'normal', 'low' ], default: 'normal'},
+  text: String,
+  html: String,
+  attachments: [ { type:String, size: Number} ],
   active: Boolean
 });
 
